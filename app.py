f491355fd10806af4d4b4fa7e634a47d4f164e92
Venkat Middey <venkatmiddey57@gmail.com>
2025-03-04 15:26:18 +0530

    Project Committed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 import os
 from flask_cors import CORS
 
+
 app = Flask(__name__)
 CORS(app)  # Enable CORS
 
@@ -12,6 +13,8 @@ model_path = os.path.join(os.path.dirname(__file__), "model/heart_model.pkl")
 with open(model_path, "rb") as file:
     model = pickle.load(file)
 
+
+
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json['features']
@@ -19,6 +22,7 @@ def predict():
     prediction = model.predict(data_array)[0]
     result = "No Heart Disease" if prediction == 0 else "Risk of Heart Disease"
     return jsonify({"prediction": result})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
