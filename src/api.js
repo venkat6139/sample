ae478b9c486d1b4e365f1b595ae763448a41c546
Venkat Middey <venkatmiddey57@gmail.com>
2025-03-04 16:17:44 +0530

    Project Committed

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:5000", // Update with deployed backend URL
+    baseURL: "https://sample-1-ep8b.onrender.com", // Update with deployed backend URL
     headers: { "Content-Type": "application/json" },
 });
 
